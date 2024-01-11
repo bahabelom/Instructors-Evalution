fde636058c06766d1737786d90c61a59b6d4903f
bahabelom <54621683+bahabelom@users.noreply.github.com>
2024-01-11 02:18:20 -0800

    Update server.js

--- server/backend/server.js
+++ server/backend/server.js
@@ -9,11 +9,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
-const corsOptions = {
-  origion: "*",
-  Credentials: true,
-  optionSuccessStatus: 200,
-};
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
